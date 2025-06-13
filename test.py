bfa932ca21a473b2d6bc9114b35cad4946b23023
aizh27 <aizhaysh@gmail.com>
2025-06-13 12:05:55 +0530

    Update test.py

--- test.py
+++ test.py
@@ -44,7 +44,7 @@ if uploaded_file:
 
     # Set up the Gemini model
     llm = ChatGoogleGenerativeAI(
-        model="gemini-2.0-flash-lite",
+        model="gemini-2.0-flash",
         temperature=0.3,
         convert_system_message_to_human=True,
     )
